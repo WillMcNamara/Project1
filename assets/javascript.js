69b75b88e5eef2c2c65eef62357db017be1d5354
Will M <wmcna97@gmail.com>
2019-04-12 11:51:45 -0400

    working directions destination

--- assets/javascript.js
+++ assets/javascript.js
@@ -4,7 +4,7 @@ function myMap() {
       zoom: 10,
       center: myLatlng
     };
-    var map = new google.maps.Map(document.getElementById("map"),mapOptions);
+    var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
     var marker = new google.maps.Marker({
         position: myLatlng,
@@ -17,26 +17,83 @@ $(document).ready(function(){
 
 //on click function for searching directions
 
-//generate map
-
-
-
+$("#destination-searchBtn").on("click", function(){
 
 //grab info from google API
 
-//get user location somehow
-// var origin = "philadelphia";
-// // var destination = $("#destination-input").val();
-// var destination = "wildwood";
+// get user location somehow
+var origin = "philadelphia";
+// var destination = $("#destination-input").val();
+var destination = $("#destination-search").val();
+console.log(destination);
 
-// // var queryURL = "https://maps.googleapis.com/maps/api/directions/json?origin=" + origin + "&destination=" + destination + "&key=AIzaSyDSIy69ZFze3YohnxsdisMinUBnct886_k";
-// var queryURL = "https://maps.googleapis.com/maps/api/directions/json?origin=philadelphia&destination=wildwood&key=AIzaSyDSIy69ZFze3YohnxsdisMinUBnct886_k"
+var queryURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/directions/json?origin=philadelphia&destination=" + destination +"&key=AIzaSyDSIy69ZFze3YohnxsdisMinUBnct886_k"
+console.log(queryURL);
 
-// $.ajax ({
-//     url: queryURL,
-//     method: "GET"
-// }).then(function(response){
-//     console.log(response)
-// })
+$.ajax ({
+    url: queryURL,
+    method: "GET"
+}).then(function(response){
+    console.log(response)
+    console.log(response.routes[0].legs[0].start_location.lat)
+    var oriLat = response.routes[0].legs[0].start_location.lat
+    var oriLng = response.routes[0].legs[0].start_location.lng
+    var desLat = response.routes[0].legs[0].end_location.lat
+    var desLng = response.routes[0].legs[0].end_location.lng
+
+    var originCoord = new google.maps.LatLng(oriLat, oriLng)
+    var destinationCoord = new google.maps.LatLng(desLat, desLng)
+    
+    //copied from stackoverflow
+    function initMap() {
+      var pointA = originCoord
+        pointB = destinationCoord
+        myOptions = {
+          zoom: 7,
+          center: pointA
+        },
+        map = new google.maps.Map(document.getElementById('map'), myOptions),
+        // Instantiate a directions service.
+        directionsService = new google.maps.DirectionsService,
+        directionsDisplay = new google.maps.DirectionsRenderer({
+          map: map
+        }),
+        markerA = new google.maps.Marker({
+          position: pointA,
+          title: "point A",
+          label: "A",
+          map: map
+        }),
+        markerB = new google.maps.Marker({
+          position: pointB,
+          title: "point B",
+          label: "B",
+          map: map
+        });
+    
+      // get route from A to B
+      calculateAndDisplayRoute(directionsService, directionsDisplay, pointA, pointB);
+    
+    }
+    
+    
+    
+    function calculateAndDisplayRoute(directionsService, directionsDisplay, pointA, pointB) {
+      directionsService.route({
+        origin: pointA,
+        destination: pointB,
+        travelMode: google.maps.TravelMode.DRIVING
+      }, function(response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+          directionsDisplay.setDirections(response);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
+    }
+    initMap();
+  
+})
+})
 })
 
